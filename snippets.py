65945711915424c17e777062c83597001e9979ec
Matthew Conquergood <matthew.conquergood@gmail.com>
2015-02-26 00:07:11 +0000

    Subparsers Added

--- snippets.py
+++ snippets.py
@@ -1,6 +1,6 @@
 import logging
 import sys
-import argparse as ap
+import argparse
 
 # Set the log output file, and the log level
 logging.basicConfig(filename="snippets.log", level=logging.DEBUG)
@@ -36,9 +36,24 @@ def dir(mask="?"):
 def main():
     """Main function"""
     logging.info("Constructing parser")
-    parser = ap.ArgumentParser(description="Store and retrieve snippets of text")
-    arguments = parser.parse_args(sys.argv[1:])
+    parser = argparse.ArgumentParser(description="Store and retrieve snippets of text")
 
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+
+    # Subparser for the put command
+    logging.debug("Constructing put subparser")
+    put_parser = subparsers.add_parser("put", help="Store a snippet")
+    put_parser.add_argument("name", help="The name of the snippet")
+    put_parser.add_argument("snippet", help="The snippet text")
+    
+    #Subparser for the get command
+    logging.debug("Constructing get subparser")
+    get_parser = subparsers.add_parser("get", help="Retrieve a snippet")
+    get_parser.add_argument("name", help="The name of the snippet")
+    
+    
+    
+    arguments = parser.parse_args(sys.argv[1:])
     
 if __name__ == "__main__":
     main()
